ffdedf2b20429f4718c8953dc47d9312e05cb727
Balogh Barnabás <balogh.barnabas2008@gmail.com>
2025-06-26 12:04:36 +0200

    IOS app project created

---
+++ ios_app/shareify/shareify/shareifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  shareifyApp.swift
+//  shareify
+//
+//  Created by Balogh Barnabás on 2025. 06. 26..
+//
+
+import SwiftUI
+
+@main
+struct shareifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios_app/shareify/shareifyTests/shareifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  shareifyTests.swift
+//  shareifyTests
+//
+//  Created by Balogh Barnabás on 2025. 06. 26..
+//
+
+import Testing
+@testable import shareify
+
+struct shareifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
